5eaba908afe168107b9d25a851bbe097949977d6
Syed Mohammad Yasir <smy329@gmail.com>
2023-01-18 11:32:41 +0600

    class-10-handle-changing-cart-items

--- README.md
+++ README.md
@@ -37,3 +37,7 @@
     2. use context to get cart items
     3. list items in cart items
     4. redirect to cart screen after add to cart
+
+10. Handle Changing Cart Items
+    1. add select box for quantity
+    2. handle select box change
--- pages/cart.js
+++ pages/cart.js
@@ -19,6 +19,16 @@ export default function CartScreen() {
       payload: item
       })
   }
+
+  const updateCartHandler = (item, qty) => {
+    //qty string is a string at option. We need to convert the qty string to number.  
+    const quantity = Number(qty)
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        ...item, quantity
+      }})
+  }
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
@@ -56,32 +66,55 @@ export default function CartScreen() {
                         {item.name}
                       </Link>
                     </td>
-                    <td className="p-5 text-right">{item.quantity}</td>
+                    <td className="p-5 text-right">
+                      <select
+                        //show the quantity as default value of dropdown
+                        value={item.quantity}
+                        //get the selected value
+                        onChange={(e) =>
+                          //pass the item and selected value
+                          updateCartHandler(item, e.target.value)
+                        }
+                      >
+                        {
+                          //create an array based on 'countInStock'. if countInStock = 5, then array will contain 0-4. key() will return 0, 1, 2, 3, 4. And start mapping
+                          [...Array(item.countInStock).keys()].map((x) => (
+                            //array starts from 0, but option need ti start from 1
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))
+                        }
+                      </select>
+                    </td>
                     <td className="p-5 text-right">{item.price}</td>
-                        <td className="p-5 text-center">
-                            <button onClick={() => removeItemHandler(item)}>
-                                <XCircleIcon className='h-5 w-5'></XCircleIcon>
-                            </button>
+                    <td className="p-5 text-center">
+                      <button onClick={() => removeItemHandler(item)}>
+                        <XCircleIcon className="h-5 w-5"></XCircleIcon>
+                      </button>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-          <div className='card p-5'>
-              <ul>
-                <li>
-                  <div className='pb-3 text-xl'>
-                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)})
-                    {' '}
-                    : ${cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
-                  </div>
-                </li>
-                <li>
-                  <button
-                    className='primary-button w-full'
-                  onClick={() => router.push('/shipping')}> CheckOut</button>
-                </li>
+          <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
+                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                </div>
+              </li>
+              <li>
+                <button
+                  className="primary-button w-full"
+                  onClick={() => router.push('/shipping')}
+                >
+                  {' '}
+                  CheckOut
+                </button>
+              </li>
             </ul>
           </div>
         </div>
